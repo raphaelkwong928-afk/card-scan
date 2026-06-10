a671ae8efdb859e1318bf869c9f76a8b6a4e44ff
Mavis <mavis@minimax.io>
2026-06-10 10:52:24 +0000

    Add Electron desktop app wrapper for Windows .exe
    - electron/main.js: Electron wrapper that loads the web app in a native window
    - package.json: electron-builder config for Windows build
    - .gitignore: ignore release/ folder

---
+++ electron/main.js
@@ -0,0 +1,41 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    minWidth: 900,
+    minHeight: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+    icon: path.join(__dirname, 'icon.png'),
+    title: 'CardScan',
+  });
+
+  // Load the built web app
+  if (process.env.NODE_ENV === 'development') {
+    win.loadURL('http://localhost:5173');
+  } else {
+    win.loadFile(path.join(__dirname, '../dist/index.html'));
+  }
+
+  // Remove default menu bar for cleaner look
+  Menu.setApplicationMenu(null);
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
